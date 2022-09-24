e85e182e15d4a616b96070e53d1d28bf90ed1e72
Uhh <102762345+0x157@users.noreply.github.com>
2022-09-23 22:25:50 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 
+
 using namespace std;
+
 
 int main()
 {
-  
-  cout << " " << endl; 
-  
-  return 0;
-  
+
+    cout << "Trying to install Python3" << endl;
+
+    system("sudo apt install python3");
+
+    return 0;
+
 }
 
