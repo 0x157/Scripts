af3bdb1538d09acd0c77c0d3689d19e368fbd75e
Red <102762345+0x157@users.noreply.github.com>
2023-01-26 14:12:31 -0500

    sl

--- linux_script.py
+++ linux_script.py
@@ -388,7 +388,7 @@ def installing_tools():
 def installing_misc():
     
     programs = [
-        "lolcat", "figlet", "cowsay", "cmatrix", "pi", "neofetch", "piper"
+        "lolcat", "figlet", "cowsay", "cmatrix", "pi", "neofetch", "piper", "sl"
     ]
 
 
