aac3ce2c6ca03707acd1f4b955f32bf37a5d5625
aleksa <zabumaphu@gmail.com>
2023-05-28 22:28:10 -0400

    First pass of clean ups made.

--- install_vol2.py
+++ install_vol2.py
@@ -1,61 +1,67 @@
-import os, subprocess
+"""
+Author: 0x157
+Description: A python script made to instal volatility2 on Linux Systems. 
+Script needs to be ran with super user priveledges.
+"""
 
-# Script does need to be ran with sudo or root
+import os
+import subprocess
+import progressBar
+import time
+import threading
 
-def install_volatility2() -> None:
-    clear = lambda: os.system('clear')
+def update():
+    try:
+        subprocess.call(["sudo", "apt-get", "update"], 
+                        stdout=subprocess.DEVNULL,
+                        stderr=subprocess.STDOUT)
+        time.sleep(1)
+    except:
+        print("System update failed.")
+
+def install_python2():
+    python2_commands = ["apt install -y python2.7", 
+                        "apt install -y python-pip python-setuptools build-essential python2.7-dev"]
+
+    subprocess.call(["apt", "install", "-y", "python2.7"],stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT )
+    subprocess.call(["apt", "install", "-y", "python-pip", "python-setuptools", "build-essential", "python2.7-dev" ],stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT )
+
+
+def clone_volatility():
+    pass
+
+def install_volatility():
+    pass
+
+def clone_and_install() -> bool:
+    print("Installing volatility.")
+    commands = ["python2 -m pip install distorm3", "python2 -m pip install pycrypto",
+                "git clone https://github.com/volatilityfoundation/volatility", 
+                "sudo mv volatility/ /opt"]
     
-    def update() -> bool:
+    for fcmd in commands:
         try:
-            subprocess.call(["sudo", "apt-get", "update"])
+            os.system(f"{fcmd}")
+        except IOError:
+            print("Incorrect system priveledges. Please run with sudo")
+        except Exception as e:
+            print(f"{e}")
 
-            clear()        
+    print(f"[+] VOL2 Should be installed.\nCheck /opt dir")
+    return True
 
-            return True
-        except:
-            pass
-    
+
+def install_volatility2():
     update()
-
-
-    def install_python2() -> None:
-        python2_commands = ["apt install -y python2.7", 
-                        "apt install -y python-pip python-setuptools build-essential python2.7-dev"
-                        ]
-
-        for command in python2_commands:
-            os.system(f"{command}")
-
-        clear()
-
-    
     install_python2()
-
-
-    def clone_and_install() -> bool:
-        commands = ["python2 -m pip install distorm3", "python2 -m pip install pycrypto",
-                    "git clone https://github.com/volatilityfoundation/volatility", 
-                    "sudo mv volatility/ /opt"]
-        
-        for fcmd in commands:
-            try:
-                os.system(f"{fcmd}")
-                clear()
-
-            except Exception as e:
-                print(f"{e}")
-
-        print(f"[+] VOL2 Should be installed.\nCheck /opt dir")
-        return True
-
     clone_and_install()
-
-
-if __name__ == "__main__":
+    
+def main():
     try:
         install_volatility2()
-
     except:
         print(f"Failed to Execute {__name__}\nExiting...")
-        raise SystemExit
-    
+    raise SystemExit
+
+if __name__ == "__main__":
+    main()
---
+++ progressBar.py
@@ -0,0 +1,46 @@
+"""
+Author: Alternative Al
+Description: A simple python loading bar for activities.
+"""
+
+import subprocess
+import time, sys
+import threading
+
+class progressBar(threading.Thread):
+    def __init__(self, message):
+         super(progressBar, self).__init__()
+         self.kill = False
+         self.stop = False
+         self.message = message
+    
+    def setMsg(self, msg):
+         self.message = msg
+         sys.stdout.flush()
+         sys.stdout.write(msg)
+
+
+    def run(self):
+            global stop
+            global kill
+            print(self.message),
+            sys.stdout.flush()
+            i = 0
+            while self.stop != True:
+                    if (i%4) == 0: 
+                        sys.stdout.write('\b/')
+                    elif (i%4) == 1: 
+                        sys.stdout.write('\b-')
+                    elif (i%4) == 2: 
+                        sys.stdout.write('\b\\')
+                    elif (i%4) == 3: 
+                        sys.stdout.write('\b|')
+
+                    sys.stdout.flush()
+                    time.sleep(0.2)
+                    i+=1
+
+            if self.kill == True: 
+                print ('\b\b\b\b ABORT!'),
+            else: 
+                print('\b\b\bDone!'),
